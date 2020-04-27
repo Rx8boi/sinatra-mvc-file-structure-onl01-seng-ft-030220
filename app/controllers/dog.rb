188b1957745cd580b8046af48a3cc5f9b5cc96e3
Charles Butler <cdbutle@outlook.com>
2020-04-27 05:46:50 +0000

    Automatically backed up by Learn

--- app/controllers/dog.rb
+++ app/controllers/dog.rb
@@ -2,9 +2,18 @@ class Dog < Sinatra::Base
 
     attr_accessor :name, :age, :breed
 
+    @@all = []
+
+
   def initialize(name, age, breed)
     self.name = name
     self.age = age
     self.breed = breed
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+    
 end
