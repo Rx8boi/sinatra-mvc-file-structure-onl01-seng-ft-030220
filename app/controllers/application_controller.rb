0c14cb8f5a82d7d6ebd3dc09a7be7a8c89e897b6
Charles Butler <cdbutle@outlook.com>
2020-04-27 05:41:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,5 @@
 class ApplicationController < Sinatra::Base
 
-  class Dog
-    attr_reader :name, :age, :breed
-
-    def initialize
-      @name = name
-      @age = age
-      @breed = breed
-    end
-
-  end
-
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
